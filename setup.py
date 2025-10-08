1c7ad68ac92c10c484016474fb104befcba30c28
Gaspard Ringuenet <gaspard.ringuenet@gmail.com>
2025-10-08 11:31:29 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using computer vision to extract backscattering layers and caracterise deep pelagic "acoustic communities" in the Amazon river plume region.',
+    author='Gaspard Ringuenet',
+    license='MIT',
+)
